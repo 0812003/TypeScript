271d670d27934000c93d05952f7b4f129d509651
Soham <sohamsangare2003@gmail.com>
2025-06-12 10:22:47 +0530

    Core Types-TypeScript

---
+++ L2/app.js
@@ -0,0 +1,35 @@
+//Array, Object, Tuple, Enum
+console.log("Object.....");
+// const person:{name:string,age:number} = {
+//     name:"Soham",
+//     age:22,
+// }
+// console.log(person);
+// console.log("Array.....");
+// const person:{name:string,age:number,skills:string[]} = {
+//     name:"Soham",
+//     age:22,
+//     skills:["MERN","Next.js"]
+// }
+// console.log(person);
+// console.log("Any.....");
+// let value:any="hello";
+// value=21;
+// value=true;
+// console.log("Tuple....."); //fixed size array;
+// let person: [string, number] = ["Soham", 21];
+// console.log("Enum.....");
+// enum UserRole {
+//   Admin,
+//   Editor,
+//   Viewer
+// }
+// function checkAccess(role: UserRole) {
+//   if (role === UserRole.Admin) {
+//     console.log("Full Access");
+//   } else {
+//     console.log("Limited Access");
+//   }
+// }
+// checkAccess(UserRole.Editor); // 
+// checkAccess(1);               // (since enums are numbers)
